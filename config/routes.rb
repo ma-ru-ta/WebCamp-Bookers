70b55d70f190c7027e25c4a2edeef1ffe71b9795
EC2 Default User <ec2-user@ip-172-31-19-133.ap-northeast-1.compute.internal>
2023-10-19 08:27:08 +0000

    [finish]テスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get '/top' => 'homes#top'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  root to: 'homes#top'
+  resources :books
 end
